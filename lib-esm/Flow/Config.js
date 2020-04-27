26fcc4ffbc3483073ae5ac030fa8fbb838357ffc
Oppodelldog <odog@netcologne.de>
2020-04-27 22:49:58 +0200

    add build

--- lib-esm/Flow/Config.js
+++ lib-esm/Flow/Config.js
@@ -1,11 +1,10 @@
-import statesDotFile from "./states.dot";
 var Config = /** @class */ (function () {
     function Config() {
     }
     Config.prototype.load = function () {
         var _this = this;
         var configItems = [];
-        var configString = statesDotFile;
+        var configString = "\ndigraph {\n    \"Idle\"         -> \"Touched\"      [ label = \"Touch Start\" ]\n    \"Touched\"      -> \"Move Diagram\" [ label = \"Touch Move on Diagram\" ]\n    \"Touched\"      -> \"Move Node\"    [ label = \"Touch Move on Node\" ]\n    \"Touched\"      -> \"Move Port\"    [ label = \"Touch Move on Port\" ]\n    \"Touched\"      -> \"Pinch Zoom\"   [ label = \"Double Touch Move\" ]\n    \"Touched\"      -> \"Idle\"         [ label = \"Touch End\" ]\n    \"Move Diagram\" -> \"Pinch Zoom\"   [ label = \"Double Touch Move\" ]\n    \"Move Diagram\" -> \"Idle\"         [ label = \"Release Diagram\" ]\n    \"Move Node\"    -> \"Pinch Zoom\"   [ label = \"Double Touch Move\" ]\n    \"Move Node\"    -> \"Idle\"         [ label = \"Release Node\" ]\n    \"Move Port\"    -> \"Pinch Zoom\"   [ label = \"Double Touch Move\" ]\n    \"Move Port\"    -> \"Idle\"         [ label = \"Release Port\" ]\n    \"Pinch Zoom\"   -> \"Touched\"      [ label = \"Touch End\" ]\n\n    \"Idle\" -> \"Mouse Zooming\" [ label = \"Use Mousewheel\" ]\n    \"Mouse Zooming\" -> \"Idle\" [ label = \"Zoom finished\" ]\n\n    \"Idle\" -> \"Adjusting Focus\" [ label = \"Double Click\" ]\n    \"Adjusting Focus\" -> \"Idle\" [ label = \"Focus adjustment finished\" ]\n\n    \"Idle\" -> \"Selecting Nodes\" [ label = \"Select Node\" ]\n    \"Selecting Nodes\" -> \"Idle\" [ label = \"Single Selection Return\" ]\n\n    \"Idle\" -> \"Selecting multiple Nodes\" [ label = \"Turn on multi selection\" ]\n    \"Selecting multiple Nodes\" -> \"Selecting multiple Nodes\" [ label = \"Select one more Node\" ]\n    \"Selecting multiple Nodes\" -> \"Selecting multiple Nodes\" [ label = \"Deselect one Node\" ]\n    \"Selecting multiple Nodes\" -> \"Idle\" [ label = \"Turn off multi selection\" ]\n\n    \"Idle\" -> \"Deleting Selected Nodes\" [ label = \"Delete Nodes\" ]\n    \"Deleting Selected Nodes\" -> \"Idle\" [ label = \"Nodes Deleted\" ]\n}\n        ";
         var regex = /(?<FromState>"*[a-zA-Z ]*"*)\s*->\s*(?<ToState>"*[a-zA-Z ]*"*)\s*\[\s*label\s*=\s*(?<Transition>"*[a-zA-Z ]*"*)\s*]/gm;
         var m;
         configString.split('\n').forEach(function (line) {
--- src/Flow/Config.ts
+++ src/Flow/Config.ts
@@ -1,5 +1,3 @@
-import statesDotFile from "./states.dot"
-
 export interface ConfigItem {
     StateFrom: string
     StateTo: string
@@ -9,8 +7,41 @@ export interface ConfigItem {
 export class Config {
     public load(): ConfigItem[] {
         let configItems = [] as ConfigItem[];
-        console.log("statesDotFile", statesDotFile);
-        const configString = statesDotFile;
+
+        const configString = `
+digraph {
+    "Idle"         -> "Touched"      [ label = "Touch Start" ]
+    "Touched"      -> "Move Diagram" [ label = "Touch Move on Diagram" ]
+    "Touched"      -> "Move Node"    [ label = "Touch Move on Node" ]
+    "Touched"      -> "Move Port"    [ label = "Touch Move on Port" ]
+    "Touched"      -> "Pinch Zoom"   [ label = "Double Touch Move" ]
+    "Touched"      -> "Idle"         [ label = "Touch End" ]
+    "Move Diagram" -> "Pinch Zoom"   [ label = "Double Touch Move" ]
+    "Move Diagram" -> "Idle"         [ label = "Release Diagram" ]
+    "Move Node"    -> "Pinch Zoom"   [ label = "Double Touch Move" ]
+    "Move Node"    -> "Idle"         [ label = "Release Node" ]
+    "Move Port"    -> "Pinch Zoom"   [ label = "Double Touch Move" ]
+    "Move Port"    -> "Idle"         [ label = "Release Port" ]
+    "Pinch Zoom"   -> "Touched"      [ label = "Touch End" ]
+
+    "Idle" -> "Mouse Zooming" [ label = "Use Mousewheel" ]
+    "Mouse Zooming" -> "Idle" [ label = "Zoom finished" ]
+
+    "Idle" -> "Adjusting Focus" [ label = "Double Click" ]
+    "Adjusting Focus" -> "Idle" [ label = "Focus adjustment finished" ]
+
+    "Idle" -> "Selecting Nodes" [ label = "Select Node" ]
+    "Selecting Nodes" -> "Idle" [ label = "Single Selection Return" ]
+
+    "Idle" -> "Selecting multiple Nodes" [ label = "Turn on multi selection" ]
+    "Selecting multiple Nodes" -> "Selecting multiple Nodes" [ label = "Select one more Node" ]
+    "Selecting multiple Nodes" -> "Selecting multiple Nodes" [ label = "Deselect one Node" ]
+    "Selecting multiple Nodes" -> "Idle" [ label = "Turn off multi selection" ]
+
+    "Idle" -> "Deleting Selected Nodes" [ label = "Delete Nodes" ]
+    "Deleting Selected Nodes" -> "Idle" [ label = "Nodes Deleted" ]
+}
+        `;
         const regex = /(?<FromState>"*[a-zA-Z ]*"*)\s*->\s*(?<ToState>"*[a-zA-Z ]*"*)\s*\[\s*label\s*=\s*(?<Transition>"*[a-zA-Z ]*"*)\s*]/gm;
         let m;
 
